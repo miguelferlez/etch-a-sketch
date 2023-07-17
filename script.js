a83b1f9e3b91f4f9bc865d3bb9f74c6fffb41c9b
miguelferlez <miguelferlez@gmail.com>
2023-07-17 14:14:54 +0200

    Add grid

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function init() {
+const grid = document.querySelector('.grid');
 
+function init() {
+    setGrid(64);
+}
+
+function setGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            grid.appendChild(cell);           
+        }
+    }
 }
 
 window.addEventListener('load', init);
