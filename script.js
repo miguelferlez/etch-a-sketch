083bb41901655b69ea8a15748183229578cf4d0a
miguelferlez <miguelferlez@gmail.com>
2023-07-27 17:10:41 +0200

    Add eraser and random color draw features

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const grid = document.querySelector('.grid');
 const gridSizeSpans = document.getElementsByClassName('size');
+
 const modal = document.querySelector('.modal');
 const resizeModal = document.querySelector('.resize-modal');
+
 const applyButton = document.querySelector('.apply-button');
 const resizeButton = document.querySelector('.resize-button');
 const rangeInput = document.querySelector('#rangeInput');
+const rainbowButton = document.querySelector('.rainbow-button');
+const eraserButton = document.querySelector('.eraser-button');
 const clearButton = document.querySelector('.clear-button');
+
 let newSize = 0;
 let size = 16;
+let modeIsEraser = false;
+let modeIsRainbow = false;
 let mouseIsDown = false;
 let mouseIsUp = true;
 
 function init() {
     newSize = size;
-    resizeButton.addEventListener('click', showResizeModal);
-    applyButton.addEventListener('click', resizeGrid);
-    clearButton.addEventListener('click', reloadGrid);
+
+    setGrid(size);
+
     for (let i = 0; i < gridSizeSpans.length; i++) {
         gridSizeSpans[i].textContent = size;
     }
-    setGrid(size);
+
+    resizeButton.addEventListener('click', showResizeModal);
+    applyButton.addEventListener('click', resizeGrid);
+
+    rainbowButton.addEventListener('click', () => {
+        rainbowButton.classList.toggle('clicked');
+        modeIsRainbow = !modeIsRainbow;
+        disableEraserMode();
+    })
+    eraserButton.addEventListener('click', () => {
+        eraserButton.classList.toggle('clicked');
+        modeIsEraser = !modeIsEraser;
+        disableRainbowMode();
+    })
+    clearButton.addEventListener('click', reloadGrid);
     rangeInput.addEventListener('input', () => {
         newSize = document.getElementById('rangeInput').value;
     })
@@ -28,8 +49,10 @@ function init() {
 function setGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
+
             let cell = document.createElement('div');
             cell.classList.add('cell');
 
@@ -42,14 +65,22 @@ function setGrid(size) {
             });
 
             grid.appendChild(cell);
+
             //Mouse events to paint the cell when holding click down and stop in click up
             cell.addEventListener('mousedown', (e) => {
                 mouseIsDown = true;
                 mouseIsUp = false;
             });
             cell.addEventListener('mousemove', (e) => {
+                let shading = 0.1;
                 if (mouseIsDown) {
-                    draw(cell, 'black');
+                    if (modeIsEraser) {
+                        draw(cell, 'white');
+                    } else if (modeIsRainbow) {
+                        draw(cell, "rgb(" + getRandomNumber(1, 255) + "," + getRandomNumber(1, 255) + "," + getRandomNumber(1, 255) + ")");
+                    } else {
+                        draw(cell, 'black');
+                    }
                 }
             });
             cell.addEventListener('mouseup', (e) => {
@@ -80,7 +111,6 @@ function showResizeModal() {
 }
 
 function resizeGrid() {
-
     for (let i = 0; i < gridSizeSpans.length; i++) {
         gridSizeSpans[i].textContent = newSize;
     }
@@ -90,6 +120,20 @@ function resizeGrid() {
 
 function closeModal() {
     modal.style.display = 'none';
+}
+
+function disableRainbowMode() {
+    modeIsRainbow = false;
+    rainbowButton.classList.remove('clicked');
+}
+
+function disableEraserMode() {
+    modeIsEraser = false;
+    eraserButton.classList.remove('clicked');
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener('load', init);
